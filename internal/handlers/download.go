1cc616bbe0ccedcee64f4e1b54431e5f704735c2
Mohamed Hesham <mohamedhesham1619@gmail.com>
2025-08-01 23:49:18 +0300

    Try to fix files serving issue on google cloud run

--- internal/handlers/download.go
+++ internal/handlers/download.go
@@ -2,11 +2,9 @@ package handlers
 
 import (
 	"fmt"
-	"io"
 	"log/slog"
-	"mime"
 	"net/http"
-	"os"
+	"net/url"
 	"path/filepath"
 	"strings"
 )
@@ -24,46 +22,14 @@ func DownloadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	slog.Info("DownloadHandler: fileId", "fileId", fileId, "filePath", filePath)
+	slog.Info("Request for DownloadHandler: ", "fileId", fileId, "filePath", filePath)
 
-	// Get file info to set Content-Length and check for existence
-	fileInfo, err := os.Stat(filePath)
-	if err != nil {
-		slog.Error("Failed to get file info, file may have been cleaned up or never existed", "filePath", filePath, "error", err)
-		http.Error(w, "File not found or unreadable", http.StatusNotFound)
-		return
-	}
+	fileName := filepath.Base(filePath)
 
-	// Open the file
-	file, err := os.Open(filePath)
+	disposition := fmt.Sprintf("attachment; filename=\"%s\"; filename*=UTF-8''%s", 
+    fileName, url.PathEscape(fileName))
 
-	if err != nil {
-		slog.Error("Error opening file", "filePath", filePath, "error", err)
-		http.Error(w, "Error opening file", http.StatusInternalServerError)
-		return
-	}
+	w.Header().Set("Content-Disposition", disposition)
 
-	defer file.Close()
-
-	// Set the content type
-	ext := filepath.Ext(filePath)
-	contentType := mime.TypeByExtension(ext)
-	if contentType == "" {
-		contentType = "application/octet-stream"
-	}
-
-	// Set the headers
-	// Add Content-Length for better client experience (e.g., download progress bar)
-	w.Header().Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
-	w.Header().Set("Content-Type", contentType)
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", fileInfo.Name()))
-
-	// Copy the file to the response writer
-	_, err = io.Copy(w, file)
-	if err != nil {
-		slog.Error("Error serving file", "ip", r.RemoteAddr, "fileId", fileId, "error", err)
-	} else {
-		slog.Info("File served successfully", "ip", r.RemoteAddr, "fileId", fileId)
-	}
-
+	http.ServeFile(w, r, filePath)
 }
